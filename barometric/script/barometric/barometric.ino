eb5ea0219c19d0ada9dd8f2047d7688ea4c45a47
seanNCTU <sean19960914.gmail.com>
2018-08-09 14:14:11 +0800

    add output temperature

--- barometric/script/barometric/barometric.ino
+++ barometric/script/barometric/barometric.ino
@@ -1,17 +1,35 @@
+/* 
+   Use BMP085 to measure pressure and temperature
+   Output format:
+   pressure temperature
+   Edited by Sean
+   Last edited: 2018/08/09
+   Changelog:
+    2018/05/04: First version
+    2018/08/09: Add output temperature
+*/
+//   Refer to: 
+// https://github.com/adafruit/Adafruit_BMP085_Unified/blob/master/examples/sensorapi/sensorapi.pde
 #include <Wire.h>
+// Download Adafruit_Sensor.h here
+// https://github.com/adafruit/Adafruit_Sensor
 #include <Adafruit_Sensor.h>
+// https://github.com/adafruit/Adafruit_BMP085_Unified
+/*********************************************************
+ * Don't forget to include libraries to your Arduino IDE * 
+ *********************************************************/
 #include <Adafruit_BMP085_U.h>
 
 // Sensor id: 10085
 Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085);
 
-/*
+/**********************
  Connect it up
- VDD <-> 3.3V
+ VDD <-> 3.3V (Not 5V!)
  GND <-> GND
  SCL <-> A5
  SDA <-> A4
- */
+ ***********************/
  
 void setup() {
   Serial.begin(9600);
@@ -29,7 +47,11 @@ void loop() {
   bmp.getEvent(&event);
   if(event.pressure)
   {
-    Serial.println(event.pressure); // Unit: hPa
+    Serial.print(event.pressure); // Unit: hPa
+    Serial.print(" ");
+    float temperature;
+    bmp.getTemperature(&temperature);
+    Serial.println(temperature);
   }
   else
   {
